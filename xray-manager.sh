e21971b2c8f560ddb69a1cb8a36a54cdf4653238
calico <guangyun1208@gmail.com>
2026-04-14 01:32:57 +0800

    Add files via upload

--- xray-manager.sh
+++ xray-manager.sh
@@ -1806,15 +1806,7 @@ function benchmark_dest() {
     BEST_DEST=""
     BEST_DEST_POOL_SIG=""
 
-    local domain_col_width=40
     local d
-    for d in "${DEST_OPTIONS[@]}"; do
-        if [[ ${#d} -gt $domain_col_width ]]; then
-            domain_col_width=${#d}
-        fi
-    done
-    domain_col_width=$((domain_col_width + 2))
-
     for d in "${DEST_OPTIONS[@]}"; do
         local times=()
         local total=0
@@ -1852,12 +1844,16 @@ function benchmark_dest() {
         [[ "$col2" != "超时" ]] && col2="${col2} ms"
         [[ "$col3" != "超时" ]] && col3="${col3} ms"
 
+        if [[ "$col1" == "超时" ]]; then col1="   超时"; fi
+        if [[ "$col2" == "超时" ]]; then col2="   超时"; fi
+        if [[ "$col3" == "超时" ]]; then col3="   超时"; fi
+
         if [[ $avg_val -lt $best_avg ]]; then
             best_avg=$avg_val
             BEST_DEST="$d"
-            printf "  ${GREEN}%-${domain_col_width}s %8s %8s %8s %9s ★${NC}\n" "$d" "$col1" "$col2" "$col3" "$avg_str"
+            printf "  ${GREEN}%-40s %7s %7s %7s %8s ★${NC}\n" "$d" "$col1" "$col2" "$col3" "$avg_str"
         else
-            printf "  %-${domain_col_width}s %8s %8s %8s %9s\n" "$d" "$col1" "$col2" "$col3" "$avg_str"
+            printf "  %-40s %7s %7s %7s %8s\n" "$d" "$col1" "$col2" "$col3" "$avg_str"
         fi
     done
 
