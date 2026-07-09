84fa0742ae7b891594ed2951e10e1de7b2fe8f3c
calico <guangyun1208@gmail.com>
2026-07-10 07:51:39 +0800

    Update xray-manager.sh

--- xray-manager.sh
+++ xray-manager.sh
@@ -7354,8 +7354,7 @@ function get_xray_version_badge() {
 function show_main_header() {
     line
     center_echo "X R A Y  M A N A G E R" "${BRIGHT_YELLOW}${BOLD}"
-    center_echo "${SCRIPT_VERSION}" "${BLUE}${BOLD}"
-    center_echo "命令: zxray" "${BLUE}${BOLD}"
+    center_echo "命令:  zxray" "${BLUE}${BOLD}"
     line
 }
 
